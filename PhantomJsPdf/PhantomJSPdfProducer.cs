a475e6bb3ae867ff6408b8515c1525ee8d95d4a3
Curtis Johnson <curtis.johnson@nfm.com>
2016-02-09 20:04:56 -0600

    initial projects and solution

---
+++ PhantomJsPdf/PhantomJSPdfProducer.cs
@@ -0,0 +1,17 @@
+﻿using Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhantomJsPdf
+{
+    public class PhantomJSPdfProducer : IPdfProducer
+    {
+        public byte[] ConvertToPdf(string html)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
